82a6891842b0f755b29472105d9ea48295e6b186
Gabriela-BR <gabrielabueno05@hotmail.com>
2022-04-08 14:06:25 -0300

    first

---
+++ AprendendoCSharp/P10-LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+
+            //0.36% = 0.0036
+            valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            Console.WriteLine("Após 1 mês, você terá R$" + valorInvestido);
+
+            valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            Console.WriteLine("Após 2 meses, você terá R$" + valorInvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
