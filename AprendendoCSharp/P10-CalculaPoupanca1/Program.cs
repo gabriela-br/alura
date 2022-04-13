514b31e15bd2c92d3bf436ff70263db3cca73338
GabrielaBR <gabrielabueno05@hotmail.com>
2022-04-13 16:17:43 -0300

    atividade2

--- AprendendoCSharp/P10-CalculaPoupanca1/Program.cs
+++ AprendendoCSharp/P10-CalculaPoupanca1/Program.cs
@@ -13,13 +13,14 @@ namespace _P10_CalculaPoupanca
             Console.WriteLine("Executando projeto 10 - Calcula poupança");
 
             double valorInvestido = 1000;
+            int contadorMes = 1;
 
-            //0.36% = 0.0036
-            valorInvestido = valorInvestido + valorInvestido * 0.0036;
-            Console.WriteLine("Após 1 mês, você terá R$" + valorInvestido);
-
-            valorInvestido = valorInvestido + valorInvestido * 0.0036;
-            Console.WriteLine("Após 2 meses, você terá R$" + valorInvestido);
+            while (contadorMes <= 12)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+                contadorMes++;
+            }
 
             Console.ReadLine();
         }
--- AprendendoCSharp/P11-CalculaPoupanca2/Program.cs
+++ AprendendoCSharp/P11-CalculaPoupanca2/Program.cs
@@ -18,8 +18,8 @@ namespace _P11_CalculaPoupanca2
             {
                 valorInvestido *= 1.0036;
                 Console.WriteLine(
-                    "Após " + contadorMes +
-                    " meses, você terá R$" + valorInvestido);
+                    "Após " + contadorMes + " meses," +
+                    " você terá R$" + valorInvestido);
             }
             Console.ReadLine();
         }
