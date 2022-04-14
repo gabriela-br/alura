d9439c079bea60ff670ca2bfb206bf7cddb850a5
GabrielaBR <gabrielabueno05@hotmail.com>
2022-04-14 15:57:56 -0300

    atividade3

---
+++ Fatorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1;
+
+            for (int x = 1; x <= 10; x++)
+            {
+                fatorial *= x;
+                Console.WriteLine(" O fatorial de " + x + " é igual a " + fatorial + ".");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LacoDeRepeticaoFor2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoDeRepeticaoFor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int multiplicador = 0; multiplicador < 10; multiplicador++)
+            {
+                for (int contador = 0; contador < 10; contador++)
+                {
+                    Console.WriteLine(multiplicador + " * " + contador + " = " + multiplicador + contador);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
